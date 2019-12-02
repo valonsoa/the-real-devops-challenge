addde42fa7349248212ccdaf32495b9e279e1a18
Víctor Alonso <valoalv2@gmail.com>
2019-12-02 11:27:28 +0100

    [Intelygenz] docker-compose + bd script

---
+++ data/script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mongod &
+while !(nc -z localhost 27017) ; do
+    sleep 2
+    echo "Waiting for mongo to initialize..."
+done
+echo 'db.createUser({user: "mongodb", pwd: "mongodb", roles: []});' > file.js
+mongo admin file.js
+mongoimport --db intelygenz --collection restaurant --authenticationDatabase admin --username mongodb --password mongodb --file ./restaurant.json
